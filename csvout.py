10a1b09ab9b297d767702f88a4c6e07ec045e9d0
kokesan1008 <46284897+Masacccch@users.noreply.github.com>
2024-11-13 12:52:52 +0000

    add price and fix date bug

--- csvout.py
+++ csvout.py
@@ -1,6 +1,10 @@
-import json
+# crip.pyで作ったjsonをCSVで出す．
 
+import csv
+import json
+import re
 from datetime import datetime
+
 
 def convert_to_datetime(nen, gatu, bi, ji, fun):
     # print(nen,gatu,bi,ji,fun,sep="-")
@@ -8,7 +12,8 @@ def convert_to_datetime(nen, gatu, bi, ji, fun):
     dt = datetime(year=nen, month=gatu, day=bi, hour=ji, minute=fun)
     is_within_range(dt)
     return dt
-    
+
+
 def is_within_range(dt):
     # 2018年10月1日から今日までの範囲を設定
     start_date = datetime(2018, 10, 1)
@@ -19,27 +24,30 @@ def is_within_range(dt):
         return True
     else:
         # print(nen,gatu,bi,ji,fun,sep="-")
+        print(dt)
         raise ValueError
         return False
-    
+
 
 def find_date(str):
-    snen = str.find('年')
-    sgatu  = str.find('月')
-    sbi= str.find('日')
-    sji =str.find('時')
-    sfun= str.find('分')
-    if(snen!=-1 and sgatu!=-1 and sbi!=-1 and sji!=-1 and sfun != -1):
-        
-        nen = int(str[snen-4:4])
-        gatu = int(str[snen+1:sgatu])
-        bi = int(str[sgatu+1:sbi])
-        ji = int(str[sbi+1:sji])
-        fun = int(str[sji+1:sfun])
-        res = convert_to_datetime(nen,gatu,bi,ji,fun)
+    snen = str.find("年")
+    sgatu = str.find("月")
+    sbi = str.find("日")
+    sji = str.find("時")
+    sfun = str.find("分")
+    if snen != -1 and sgatu != -1 and sbi != -1 and sji != -1 and sfun != -1:
+
+        nen = int(str[snen - 4 : snen])
+        gatu = int(str[snen + 1 : sgatu])
+        bi = int(str[sgatu + 1 : sbi])
+        ji = int(str[sbi + 1 : sji])
+        fun = int(str[sji + 1 : sfun])
+        # print(nen, gatu, bi, ji, fun)
+        res = convert_to_datetime(nen, gatu, bi, ji, fun)
         return res
 
-with open('out/ppd.mp4_15.json') as f:
+
+with open("out/ppdc2.mp4_20.json") as f:
     d = json.load(f)
 
 no_date_cnt = 0
@@ -50,14 +58,17 @@ l = []
 dl = []
 
 for index, di_ct in enumerate(d):
-    #print(index,type(value))
+    # print(index,type(value))
     # ここでstrでfor回す
     no_date_flg = True
     getted_date = None
     dated_num = 0
-    for j,str in enumerate(di_ct['str']):
+    # このforは店名などそもそも日付じゃないのも来る
+    for j, stri in enumerate(di_ct["str"]):
+        if index ==3 and j == 1:
+            print("stop")
         try:
-            res_date = find_date(str)
+            res_date = find_date(stri)
             if type(res_date) is datetime:
                 l.append(res_date)
                 getted_date = res_date
@@ -70,16 +81,26 @@ for index, di_ct in enumerate(d):
             # print("err")
     if no_date_flg:
         no_date_cnt += 1
+    # ちゃんと日付入ってたら
     else:
-        strcombined = di_ct['str'][:dated_num] + di_ct['str'][dated_num:] 
-        ddd = {"Date": getted_date, "Num": di_ct['Num'],"str":"aaa" ,"Price":'PRICE'}
+        strcombined = di_ct["str"][:dated_num] + di_ct["str"][dated_num + 1 :]
+        # 数字だけつまり金額だけ抜く
+        price_a = re.sub(r"\D", "",  ', '.join(strcombined))
+        ddd = {
+            "Date": str(getted_date),
+            "Num": di_ct["Num"],
+            "Price": price_a,
+            "str": strcombined,
+        }
         dl.append(ddd)
     # print("next")
-    
-    
-    dl.append(d)
 
-            
+    # dl.append(d)
 
+fieldnames = ["Date", "Num","str","Price"]
+with open("out/outcsv.csv", "w", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(dl)
 
 print("Fin")
